2a175f3b187da321dbfceb398ca609bdaec27d77
mas <mas@thorium>
2024-03-18 21:57:29 -0400

    Arguments added and help messages

--- data_ingest.py
+++ data_ingest.py
@@ -1,23 +1,23 @@
 import pandas as pd
 
 
-def read_file(file:str) -> pd.DataFrame:
+def read_file(file: str) -> pd.DataFrame:
     """
     This functions takes in a string that contains information
     Regarding the location for a strava.csv file
     """
 
     df = pd.read_csv(file)
+    print(f"Loaded {len(df)} rows of data")
     return df
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('file',
-                        type=str,
-                        help='File location for strava.csv data')
-    print('Loading data ....')
-    parser.add_argument('output',
-                        help='resultant Pandas dataframe after reading in file')
+    parser.add_argument("file", type=str, help="File location for strava.csv data")
+    args = parser.parse_args()
+
+    print("Loading data ....")
+    data = read_file(args.file)
